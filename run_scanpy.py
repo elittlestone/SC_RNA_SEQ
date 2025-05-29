eb8e7e4386953f045ee40b12bd7b9b7900190a27
Ethan Littlestone <ethanlittlestone@gmail.com>
2025-05-29 17:28:54 -0400

    scanpy python script

---
+++ run_scanpy.py
@@ -0,0 +1,16 @@
+import scanpy as sc
+import anndata as ad
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description = "Scanpy scRNA-seq processing script")
+    parser.add_argument("--input", type = str, required = True,
+                        help = "Path to ")
+    sample1 = 
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ scanpy.py
@@ -0,0 +1,14 @@
+import scanpy as sc
+import anndata as ad
+import argparse
+
+def main():
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
