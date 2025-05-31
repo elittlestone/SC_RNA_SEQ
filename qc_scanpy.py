2aaec9592b7f8ed530555dbc3dc8b2027a4836ca
Ethan Littlestone <ethanlittlestone@gmail.com>
2025-05-31 19:55:15 -0400

    Added plots

--- qc_scanpy.py
+++ qc_scanpy.py
@@ -7,13 +7,24 @@ def main():
     parser.add_argument("--input_h5ad", required=True)
     parser.add_argument("--sample_id", required=True)
     args = parser.parse_args()
-
-    adata = sc.read_h5ad(args.input_h5ad)
-
-    # Set output dir
-    outdir = "results/qc_figures"
+    
+    # Create dirs 
+    outdir = "results/figures"
     os.makedirs(outdir, exist_ok=True)
     sc.settings.figdir = outdir
+
+    adata = sc.read_h5ad(args.input_h5ad)
+    sample_id = args.sample_id
+    # Post-processing steps go here
+    adata = filter_cells(adata, sample_id)
+    adata = doublet_detection(adata)
+    adata = normalization(adata)
+    adata = feature_selection(adata, sample_id)
+    adata = dimensionality_reduction(adata, sample_id)
+
+
+
+def filter_cells(adata, sample_id):
 
     # Calculate QC metrics
     adata.var["mt"] = adata.var_names.str.startswith("MT-")
@@ -28,7 +39,7 @@ def main():
         ["n_genes_by_counts", "total_counts", "pct_counts_mt"],
         jitter=0.4,
         multi_panel=True,
-        save=f"_{args.sample_id}.png"
+        save=f"_{sample_id}.png"
     )
 
     sc.pl.scatter(
@@ -36,12 +47,39 @@ def main():
         x="total_counts",
         y="n_genes_by_counts",
         color="pct_counts_mt",
-        save=f"_{args.sample_id}.png"
+        save=f"_{sample_id}.png"
     )
     
     sc.pp.filter_cells(adata, min_genes = 100)
     sc.pp.filter_genes(adata, min_cells = 3)
 
+    return adata
+
+def doublet_detection(adata):
+    sc.pp.scrublet(adata, batch_key="sample")
+    return adata 
+
+def normalization(adata):
+    adata.layers["counts"] = adata.X.copy()
+    sc.pp.normalize_total(adata)
+    sc.pp.log1p(adata)
+    return adata
+
+def feature_selection(adata, sample_id):
+    sc.pp.highly_variable_genes(adata, n_top_genes = 2000, batch_key = "sample")
+    sc.pl.highly_variable_genes(adata, save = f"_{sample_id}.png")
+    return adata
+
+def dimensionality_reduction(adata, sample_id):
+    sc.tl.pca(adata)
+    sc.pl.pca_variance_ratio(adata, n_pcs = 50, log = True, save = f"_{sample_id}")
+    sc.pl.pca(adata,
+              color = ["sample", "sample", "pct_counts_mt", "pct_counts_mt"],
+              dimensions = [(0, 1), (2, 3), (0, 1), (2, 3)],
+              ncols = 2,
+              size = 2, 
+              save = f"_{sample_id}")
+    return adata
 if __name__ == "__main__":
     main()
 
--- scanpy.smk
+++ scanpy.smk
@@ -5,9 +5,11 @@ SAMPLES = config["samples"]
 rule all:
   input:
     expand("results/{sample}_processed.h5ad", sample = SAMPLES),
-    expand("results/qc_figures/violin_{sample}.png", sample = SAMPLES),
-    expand("results/qc_figures/scatter_{sample}.png", sample = SAMPLES)
-        
+    expand("results/figures/violin_{sample}.png", sample = SAMPLES),
+    expand("results/figures/scatter_{sample}.png", sample = SAMPLES),
+    expand("results/figures/filter_genes_dispersion_{sample}.png", sample = SAMPLES),
+    expand("results/figures/pca_variance_ratio_{sample}.pdf", sample = SAMPLES),
+    expand("results/figures/pca_{sample}.pdf", sample = SAMPLES)
 
 rule run_scanpy:
   input:
@@ -16,11 +18,11 @@ rule run_scanpy:
     h5ad = "results/{sample}_processed.h5ad"
   params:
     results_dir = "results",
-    qc_figures_dir = "results/qc_figures"
+    figures_dir = "results/figures"
   shell:
     """
     mkdir -p {params.results_dir}
-
+    mkdir -p {params.figures_dir}
     python import_10x_data.py --feature_matrix_file {input.matrix_file} \
     --sample_id {wildcards.sample} --processed_h5 {output.h5ad}
     """
@@ -29,11 +31,13 @@ rule run_qc:
   input:
     h5ad = "results/{sample}_processed.h5ad"
   params:
-    sample = "{sample}",
-    qc_dir = "results/qc_figures"
+    sample = "{sample}"
   output:
-    violin = "results/qc_figures/violin_{sample}.png",
-    scatter_plot = "results/qc_figures/scatter_{sample}.png"
+    violin = "results/figures/violin_{sample}.png",
+    scatter_plot = "results/figures/scatter_{sample}.png",
+    highly_variable_genes = "results/figures/filter_genes_dispersion_{sample}.png",
+    pca_variance_ratio = "results/figures/pca_variance_ratio_{sample}.pdf",
+    pca = "results/figures/pca_{sample}.pdf"
   shell:
     """
     python qc_scanpy.py --input_h5ad {input.h5ad} --sample_id {params.sample}
